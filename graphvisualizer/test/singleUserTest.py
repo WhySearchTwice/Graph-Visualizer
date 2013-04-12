93137b36018e7226b46d9e1054e5e909730d45ad
Tony Grosinger <tony@grosinger.net>
2013-04-12 08:45:07 -0700

    Added comments to test script.

--- graphvisualizer/test/singleUserTest.py
+++ graphvisualizer/test/singleUserTest.py
@@ -1,6 +1,14 @@
 from graphvisualizer import Visualizer
 
 v = Visualizer()
+
+# File to be read from
 v.setInputFile('largeSample.json')
+
+# ID of the user to start from. This can optionally be ommitted to render the whole graph.
 v.setUsers([92])
+
+# Start the rendering process. This may take a while
 v.runSimulation()
+
+# Leave the server running to answer XML-RPC callbacks
